89b36fd6f0d6dd6c06f7864be33dc421620af76f
Constantine <lars93.pk@gmail.com>
2024-10-01 21:57:37 +0300

    Add files via upload

---
+++ variables hometask.py
@@ -0,0 +1,6 @@
+completed_hometasks = 12
+overall_hours = 1.5
+course_name = 'Python'
+one_task_time = (overall_hours / completed_hometasks)
+
+print('Курс:',course_name,',','всего задач:',completed_hometasks,',','затрачено часов:',overall_hours,',','среднее время выполнения:',one_task_time, 'часа.')
